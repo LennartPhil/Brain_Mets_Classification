f12a3a960556e6c8d1b7822e1f75a9e0affa8729
Lennart Philipp <lennart-philipp@outlook.de>
2024-01-27 16:53:44 +0100

    adds the reorientation function to the preprocessing file

--- brain_mets_classification/preprocessing_funcs.py
+++ brain_mets_classification/preprocessing_funcs.py
@@ -1,4 +1,8 @@
 from nipype.interfaces.dcm2nii import Dcm2niix
+import pathlib
+from typing import Union, List, Tuple
+from nipype.interfaces import fsl
+
 
 def convert_dicom_to_nifti (path_to_sequence_folder: str, path_to_output_directory: str, new_filename: str):
     '''Converts a Dicom folder into a nifti file
@@ -16,3 +20,21 @@ def convert_dicom_to_nifti (path_to_sequence_folder: str, path_to_output_directo
     converter.inputs.out_filename = new_filename
     converter.inputs.output_dir = path_to_output_directory
     converter.run()
+
+def reorient_brain(
+    path_to_input_image: Union[str, pathlib.Path],
+    path_to_output_image: Union[str, pathlib.Path],
+):
+    """
+    applies FSL.Reorient2Std() to input brain scan
+    and returns reoriented image
+
+    Keyword Arguments:
+    path_to_input_image: Union[str, pathlib.Path] = file path to input image (brain scan)
+    path_to_output_image: Union[str, pathlib.Path] = location to store reoriented image
+    """
+
+    reorient = fsl.Reorient2Std()
+    reorient.inputs.in_file = path_to_input_image
+    reorient.inputs.out_file = path_to_output_image
+    reorient.run()
