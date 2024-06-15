898a96df7a16ba89742fbdf6afe099b9a6020db4
Lennart Philipp <lennart-philipp@outlook.de>
2024-06-15 17:01:01 +0200

    .

---
+++ scripts/data_preparation/data_preparation.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+import nibabel as nib
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from scipy import ndimage
+from pathlib import Path
+
+from tensorflow.train import BytesList, FloatList, Int64List
+from tensorflow.train import Feature, Features, Example
+
+from sklearn.utils import class_weight
+
+from tqdm import tqdm
+
+def data_preparation():
+    pass
+
+if __name__ == "__main__":
+    data_preparation()
