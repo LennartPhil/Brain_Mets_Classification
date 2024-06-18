fed37177582837087a023b76c343915c50639fbe
Lennart Philipp <lennart-philipp@outlook.de>
2024-06-18 10:44:24 +0200

    adds tensorflow_setup, get_callbacks

--- scripts/3D_CNN_whole/3D_CNN_test.py
+++ scripts/3D_CNN_whole/3D_CNN_test.py
@@ -1,10 +1,12 @@
 # To-do:
 # - add callbacks
+# - add class weights
 # - add ResNexT structure
 
 import tensorflow as tf
 import os
 import random
+from time import strftime
 
 from pathlib import Path
 
@@ -16,6 +18,9 @@ random.seed(42)
 
 # Variables
 path_to_tfrs = "/tfrs"
+path_to_logs = "/logs"
+
+num_classes = 2
 
 ## train / val / test split
 train_ratio = 0.8
@@ -23,29 +28,69 @@ val_ratio = 0.1
 test_ratio = 0.1
 
 batch_size = 1
+epochs = 20
+shuffle_buffer_size = 400
 
-activation_func = "relu"
+activation_func = "mish"
 optimizer = tf.keras.optimizers.legacy.SGD(learning_rate=0.001, momentum=0.9, nesterov=True)
 
 
 def train_ai():
+
+    tensorflow_setup()
+
+    # Data setup
     tfr_paths = get_tfr_paths()
     train_paths, val_paths, test_paths = split_data(tfr_paths)
     train_data, val_data, test_data = read_data(train_paths, val_paths, test_paths)
+
+    # callbacks
+    callbacks = get_callbacks()
+
     model = build_model()
-    model.fit(train_data, validation_data = val_data, epochs=20, batch_size=1,)
+    model.fit(train_data,
+              validation_data = val_data,
+              epochs = epochs,
+              batch_size = batch_size,
+              callbacks = callbacks)
+    
     score = model.evaluate(test_data)
+
+def tensorflow_setup():
+    """
+    Set up the TensorFlow environment for training.
+
+    This function sets the random seed for TensorFlow and restricts TensorFlow to use only the first GPU.
+    It also prints the number of physical and logical GPUs available.
+    """
+
+    tf.keras.utils.set_random_seed(42)
+
+    # copied directly from: https://www.tensorflow.org/guide/gpu#limiting_gpu_memory_growth
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+    
+    print("tensorflow_setup successful")
 
 def verify_tfrecord(file_path):
     try:
-        for _ in tf.dataTFRecordDataset(file_path, compression_type="GZIP"):
+        for _ in tf.data.TFRecordDataset(file_path, compression_type="GZIP"):
             pass
     except tf.errors.DataLossError:
         print(f"Corrupted TFRecord file: {file_path}")
 
 def get_tfr_paths():
     tfr_file_names = [file for file in os.listdir(path_to_tfrs) if file.endswith(".tfrecord")]
-    tfr_file_names = random.shuffle(tfr_file_names)
+    random.shuffle(tfr_file_names)
 
     tfr_paths = [str(path_to_tfrs) + "/" + file for file in tfr_file_names]
 
@@ -66,6 +111,8 @@ def split_data(tfr_paths):
     val_paths = tfr_paths[train_size:train_size + val_size]
     test_paths = tfr_paths[train_size + val_size:]
 
+    print(f"train: {len(train_paths)} | val: {len(val_paths)} | test: {len(test_paths)}")
+
     sum = len(train_paths) + len(val_paths) + len(test_paths)
     if sum != len(tfr_paths):
         print("WARNING: error occured in train / val / test split!")
@@ -82,9 +129,9 @@ def read_data(train_paths, val_paths, test_paths):
     val_data = val_data.map(partial(parse_record, labeled = True))
     test_data = test_data.map(partial(parse_record, labeled = True))
 
-    train_data = train_data.shuffle(buffer_size=100)
-    val_data = val_data.shuffle(buffer_size=100)
-    test_data = test_data.shuffle(buffer_size=100)
+    train_data = train_data.shuffle(buffer_size=shuffle_buffer_size)
+    val_data = val_data.shuffle(buffer_size=shuffle_buffer_size)
+    test_data = test_data.shuffle(buffer_size=shuffle_buffer_size)
 
     train_data = train_data.batch(batch_size)
     val_data = val_data.batch(batch_size)
@@ -128,6 +175,39 @@ def parse_record(record, labeled = False):
     else:
         return image
 
+def get_callbacks():
+    
+    class_directory = f"{num_classes}_classes"
+
+    # create callbacks directory
+    path_to_callbacks = Path(path_to_logs) / Path(class_directory)
+
+    def get_run_logdir(root_logdir= path_to_callbacks / "tensorboard"):
+        return Path(root_logdir) / strftime("run_%Y_%m_%d_%H_%M_%S")
+
+    run_logdir = get_run_logdir()
+
+    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(filepath = path_to_callbacks / "saved_weights.weights.h5",
+                                                    monitor = "val_accuracy",
+                                                    mode = "max",
+                                                    save_best_only = True,
+                                                    save_weights_only = True)
+
+    early_stopping_cb = tf.keras.callbacks.EarlyStopping(patience=10,
+                                                        restore_best_weights = True)
+
+    tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir = run_logdir,
+                                                    histogram_freq = 1)
+    
+    print("get_callbacks successful")
+
+    return [checkpoint_cb, early_stopping_cb, tensorboard_cb]
+
+# MCDropout
+# https://arxiv.org/abs/1506.02142
+class MCDropout(tf.keras.layers.Dropout):
+    def call(self, inputs, training=False):
+        return super().call(inputs, training=True)
 
 def build_model():
     batch_norm_layer = tf.keras.layers.BatchNormalization()
