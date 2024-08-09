ae9c64d239d180a7c809274a49b43e6ca5f67ea9
Lennart Philipp <lennart-philipp@outlook.de>
2024-08-09 09:46:34 +0200

    adds scripts for the training of a pretrained inceptionv3 and resnet50v2 model

---
+++ scripts/2D_CNNs/2D_CNN_cutout/2D_CNN_cutout_pretrained_inceptionv3.py
@@ -0,0 +1,234 @@
+import tensorflow as tf
+import helper_funcs as hf
+from pathlib import Path
+import os
+from time import strftime
+from functools import partial
+import numpy as np
+
+# So the idea with the pretrained models is the following:
+# 1. Run: adjust the upper layers with trainable turned off
+# !before starting 2nd run: define the path_to_weights below!
+# 2. Run: find best learning rate with exported weights
+# 3. Run: train of the best learning rate
+
+gpus = tf.config.list_physical_devices('GPU')
+print(gpus)
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+print("tensorflow_setup successful")
+
+rgb_images = True # using gray scale images as input
+num_classes = 4
+train_upper_layers = True
+use_k_fold = False
+learning_rate_tuning = False
+path_to_weights = ""
+
+batch_size = 50
+training_epochs = 400 #1000
+learning_rate = 0.01 #0.000001
+
+image_size = 299
+
+training_codename = "PT_inceptionv3_upperlayer00"
+
+path_to_tfrs = "/tfrs"
+path_to_logs = "/logs"
+
+activation_func = "mish"
+
+
+time = strftime("run_%Y_%m_%d_%H_%M_%S")
+class_directory = f"{training_codename}_{num_classes}_classes_{time}"
+path_to_callbacks = Path(path_to_logs) / Path(class_directory)
+os.makedirs(path_to_callbacks, exist_ok=True)
+
+def train_ai():
+
+    train_data, val_data, test_data = hf.setup_data(path_to_tfrs, path_to_callbacks, num_classes, batch_size = batch_size,rgb = rgb_images)
+
+    if use_k_fold:
+        pass
+    elif train_upper_layers:
+        
+        callbacks = hf.get_callbacks(path_to_callbacks, 0,
+                                     use_early_stopping = False,
+                                     stop_training = True)
+        
+        model = build_pt_inceptionv3_model()
+
+        # traing model
+        history = model.fit(
+            train_data,
+            validation_data = val_data,
+            epochs = training_epochs,
+            batch_size = batch_size,
+            callbacks = callbacks
+        )        
+
+        # save history
+        history_dict = history.history
+        history_file_name = f"history.npy"
+        path_to_np_file = path_to_callbacks / history_file_name
+        np.save(path_to_np_file, history_dict)
+
+    elif learning_rate_tuning:
+        
+        callbacks = hf.get_callbacks(path_to_callbacks, 0,
+                                     use_lrscheduler=True,
+                                     use_early_stopping=False)
+
+        # build model
+        model = build_pt_inceptionv3_model()
+
+        # load weights
+        model.load_weights(path_to_weights)
+
+        # traing model
+        history = model.fit(
+            train_data,
+            validation_data = val_data,
+            epochs = training_epochs,
+            batch_size = batch_size,
+            callbacks = callbacks
+        )        
+
+        # save history
+        history_dict = history.history
+        history_file_name = f"history.npy"
+        path_to_np_file = path_to_callbacks / history_file_name
+        np.save(path_to_np_file, history_dict)
+
+    else:
+        # regular training
+
+        # get callbacks
+        callbacks = hf.get_callbacks(path_to_callbacks, 0)
+
+        # build model
+        model = build_pt_inceptionv3_model()
+
+        # load weights
+        model.load_weights(path_to_weights)
+
+        # traing model
+        history = model.fit(
+            train_data,
+            validation_data = val_data,
+            epochs = training_epochs,
+            batch_size = batch_size,
+            callbacks = callbacks
+        )        
+
+        # save history
+        history_dict = history.history
+        history_file_name = f"history.npy"
+        path_to_np_file = path_to_callbacks / history_file_name
+        np.save(path_to_np_file, history_dict)
+    
+    tf.keras.backend.clear_session(free_memory = True)
+
+
+def build_pt_inceptionv3_model():
+
+    optimizer = tf.keras.optimizers.legacy.SGD(learning_rate=learning_rate, momentum=0.9, nesterov=True)
+
+    # Define inputs
+    image_input = tf.keras.layers.Input(shape=(240, 240, 3))
+    sex_input = tf.keras.layers.Input(shape=(1,))
+    age_input = tf.keras.layers.Input(shape=(1,))
+
+    augmented = data_augmentation(image_input)
+    
+    tf.print("Augmented shape:", augmented.shape)
+
+    # Use the pretrained base model
+    x = tf.keras.applications.InceptionV3(include_top = False)(augmented)
+    x = tf.keras.layers.GlobalMaxPool2D()(x)
+
+    output = tf.keras.layers.Flatten()(x)
+
+    # Process sex and age inputs
+    flattened_sex_input = tf.keras.layers.Flatten()(sex_input)
+    age_input_reshaped = tf.keras.layers.Reshape((1,))(age_input)
+    concatenated_inputs = tf.keras.layers.Concatenate()([output, age_input_reshaped, flattened_sex_input])
+
+    # Define dense and dropout layers
+    dense_1_layer = tf.keras.layers.Dense(256, activation='relu', kernel_initializer=tf.keras.initializers.HeNormal())
+    dropout_1_layer = tf.keras.layers.Dropout(0.5)
+    dense_2_layer = tf.keras.layers.Dense(128, activation='relu', kernel_initializer=tf.keras.initializers.HeNormal())
+    dropout_2_layer = tf.keras.layers.Dropout(0.5)
+
+    # Fully connected layers
+    x = dense_1_layer(concatenated_inputs)
+    x = dropout_1_layer(x)
+    x = dense_2_layer(x)
+    x = dropout_2_layer(x)
+
+    # Output layer
+    match num_classes:
+        case 2:
+            x = tf.keras.layers.Dense(1)(x)
+            output = tf.keras.layers.Activation('sigmoid', dtype='float32', name='predictions')(x)
+        case 3 | 4 | 5 | 6:
+            x = tf.keras.layers.Dense(num_classes)(x)
+            output = tf.keras.layers.Activation('softmax', dtype='float32', name='predictions')(x)
+        case _:
+            raise ValueError("num_classes must be 2, 3, 4, 5 or 6.")
+
+    model = tf.keras.Model(inputs = [image_input, sex_input, age_input], outputs = [output], name = "resnet34_model")
+
+    if num_classes > 2:
+        model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer, metrics = ["RootMeanSquaredError", "accuracy"])
+    else:
+        model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics = ["RootMeanSquaredError", "accuracy"])
+    
+    model.summary()
+
+    return model
+
+
+class NormalizeToRange(tf.keras.layers.Layer):
+    def __init__(self, zero_to_one=True):
+        super(NormalizeToRange, self).__init__()
+        self.zero_to_one = zero_to_one
+
+    def call(self, inputs):
+        min_val = tf.reduce_min(inputs)
+        max_val = tf.reduce_max(inputs)
+        if self.zero_to_one:
+            # Normalize to [0, 1]
+            normalized = (inputs - min_val) / (max_val - min_val)
+        else:
+            # Normalize to [-1, 1]
+            normalized = 2 * (inputs - min_val) / (max_val - min_val) - 1
+        return normalized
+    
+data_augmentation = tf.keras.Sequential([
+    tf.keras.layers.RandomFlip(mode = "horizontal"),
+    #tf.keras.layers.Rescaling(1/255),
+    tf.keras.layers.RandomContrast(0.5), # consider removing the random contrast layer as that causes pixel values to go beyond 1
+    tf.keras.layers.RandomBrightness(factor = (-0.2, 0.4)), #, value_range=(0, 1)
+    tf.keras.layers.RandomRotation(factor = (-0.1, 0.1), fill_mode = "nearest"),
+    NormalizeToRange(zero_to_one=True),
+    tf.keras.layers.RandomTranslation(
+        height_factor = 0.05,
+        width_factor = 0.05,
+        fill_mode = "nearest",
+        interpolation = "bilinear"
+    ),
+    tf.keras.layers.Resizing(image_size, image_size)
+])
+
+if __name__ == "__main__":
+    train_ai()
