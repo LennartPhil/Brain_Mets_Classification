076cc2a3342eaf6cf1035ac78b3cdca68d974a7c
Lennart Philipp <lennart-philipp@outlook.de>
2024-10-22 16:42:45 +0200

    sets max epochs to 1000, sets lr to 0.001, reduces batch_size to 10, sets use_k_fold to False, adjusts codename

--- scripts/2D_CNNs/2D_CNN_cutout/2D_CNN_cutout_resnext50.py
+++ scripts/2D_CNNs/2D_CNN_cutout/2D_CNN_cutout_resnext50.py
@@ -24,13 +24,13 @@ print("tensorflow_setup successful")
 cutout = True #if true, the metastasis is simply cutout, if false the entire slice of the brain is used
 rgb_images = False # using gray scale images as input
 num_classes = 2
-use_k_fold = True
+use_k_fold = False
 learning_rate_tuning = False
 
 
-batch_size = 20 #50
-training_epochs = 400 #1000
-learning_rate = 0.01
+batch_size = 10 #50
+training_epochs = 1000
+learning_rate = 0.001
 
 dropout_rate = 0.4
 
