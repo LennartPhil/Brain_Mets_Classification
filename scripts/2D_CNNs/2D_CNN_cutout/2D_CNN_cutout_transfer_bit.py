770bba232bb73c74e40bfbb9b5ead945fc6fd9a1
Lennart Philipp <lennart-philipp@outlook.de>
2024-10-28 16:25:02 +0100

    changes to train_upper_layers

--- scripts/2D_CNNs/2D_CNN_cutout/2D_CNN_cutout_transfer_bit.py
+++ scripts/2D_CNNs/2D_CNN_cutout/2D_CNN_cutout_transfer_bit.py
@@ -34,9 +34,9 @@ print("tensorflow_setup successful")
 cutout = True
 rgb_images = True # using gray scale images as input
 num_classes = 2
-train_upper_layers = False
+train_upper_layers = True
 use_k_fold = False
-learning_rate_tuning = True
+learning_rate_tuning = False
 
 batch_size = 20
 training_epochs = 400 #1000 #500
