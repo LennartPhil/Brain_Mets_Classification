1b5adfd4299ed5b5b59062a65f157a8ff0aaab19
Lennart Philipp <lennart-philipp@outlook.de>
2024-11-26 17:24:11 +0100

    increases dropout to 0.45, decreases learing rate to 0.0005

--- scripts/2D_CNNs/2D_CNN_cutout/2D_CNN_cutout_resnext50.py
+++ scripts/2D_CNNs/2D_CNN_cutout/2D_CNN_cutout_resnext50.py
@@ -33,10 +33,10 @@ batch_size = 20 #50
 if learning_rate_tuning:
     training_epochs = 400
 else:
-    training_epochs = 1000
-learning_rate = 0.001
+    training_epochs = 1500
+learning_rate = 0.0005
 
-dropout_rate = 0.4 #0.5
+dropout_rate = 0.45 #0.5
 
 codename = "resnext50_00"
 training_codename = hf.get_training_codename(
