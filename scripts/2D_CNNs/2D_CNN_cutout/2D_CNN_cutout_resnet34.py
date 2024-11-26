da7dcbece85ebd2566b565c745c261247012ada6
Lennart Philipp <lennart-philipp@outlook.de>
2024-11-26 17:23:52 +0100

    increases dropout to 0.5

--- scripts/2D_CNNs/2D_CNN_cutout/2D_CNN_cutout_resnet34.py
+++ scripts/2D_CNNs/2D_CNN_cutout/2D_CNN_cutout_resnet34.py
@@ -33,11 +33,11 @@ batch_size = 50
 if learning_rate_tuning:
     training_epochs = 400
 else:
-    training_epochs = 1000
+    training_epochs = 1500
  # for learning rate set to training_epochs to 400
 learning_rate = 0.001 #0.0001
 
-dropout_rate = 0.4 #0.6
+dropout_rate = 0.5 #0.6
 
 codename = "resnet34_00"
 training_codename = hf.get_training_codename(
