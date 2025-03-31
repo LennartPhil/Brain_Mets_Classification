f39a0ffdcbb81b38240ade9cd4a321af64b683e1
Lennart Philipp <lennart-philipp@outlook.de>
2025-03-31 16:49:50 +0200

    adds a constants.py file to store all the constants in

---
+++ scripts/2D_CNNs/constants.py
@@ -0,0 +1,63 @@
+from enum import Enum, auto
+
+# --- Path constants ---
+path_to_logs = "/logs"
+path_to_tfr_dirs = "/tfrs"
+path_to_splits = "/tfrs/split_text_files" # Directory containing train/val/test split files
+paths_to_rough_pretraining = "/rough_pretraining/rough_train.tfrecord", "/rough_pretraining/rough_val.tfrecord" #"/tfrs/rough_pretraining/train.tfrecord", "tfrs/rough_pretraining/val.tfrecord"
+
+# --- Data setup constants ---
+train_ratio = 0.8
+val_ratio = 0.1
+test_ratio = 0.1
+
+shuffle_buffer_size = 200
+repeat_count = 1
+
+AGE_MIN = 0
+AGE_MAX = 110
+
+LAYER_MIN = 0
+LAYER_MAX = 154
+
+IMG_SIZE = 240
+
+ROUGH_NUM_IMAGES = 3 # 3 rgb images
+
+# --- Sequence / Channel Mapping ---
+SEQUENCE_TO_INDEX = {
+    "t1": 0,
+    "t1c": 1,
+    "t2": 2,
+    "flair": 3,
+    "mask": 4,
+}
+# Example: selected_sequences = ["t1c", "flair", "mask"] -> indices = [1, 3, 4]
+
+# --- Training constants ---
+kernel_initializer = "he_normal"
+activation_func = "mish"
+
+early_stopping_patience = 300 #200
+
+# --- Model constants ---
+# INPUT_SHAPE_RGB = (IMG_SIZE, IMG_SIZE, 3)
+# INPUT_SHAPE_4_SEQ = (IMG_SIZE, IMG_SIZE, 4)
+# INPUT_SHAPE_4_SEQ_MASK = (IMG_SIZE, IMG_SIZE, 5) 
+
+# --- Weights constants ---
+#two_class_weights = {1: 0.92156863, 0 :1.09302326}
+normal_two_class_weights = {0: 1.09302326, 1: 0.92156863}
+
+# --- Type of dataset to use ---
+class Dataset(Enum):
+    NORMAL = auto()
+    PRETRAIN_ROUGH = auto() # external brain_tumor_dataset
+    PRETRAIN_FINE = auto() # external high quality brain leasion dataset
+
+# --- Type of training to run ---
+class Training(Enum):
+    LEARNING_RATE_TUNING = auto()
+    NORMAL = auto() # Standard training on one split
+    K_FOLD = auto() # K-Fold cross-validation
+    UPPER_LAYER = auto() # Transfer Learning
