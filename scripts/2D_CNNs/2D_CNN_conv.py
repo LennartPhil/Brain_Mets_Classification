e343087f4dd14dd50be219dcf14722c9660c0dee
Lennart Philipp <lennart-philipp@outlook.de>
2025-07-12 16:42:27 +0200

    sets RGB_images to False, sets use_layer to True, sets use_pretrained_weights to False

--- scripts/2D_CNNs/2D_CNN_conv.py
+++ scripts/2D_CNNs/2D_CNN_conv.py
@@ -25,16 +25,16 @@ print("tensorflow_setup successful")
 
 # --- Configuration ---
 dataset_type = constants.Dataset.NORMAL # PRETRAIN_ROUGH, PRETRAIN_FINE, NORMAL
-training_mode = constants.Training.NORMAL # LEARNING_RATE_TUNING, NORMAL, K_FOLD, UPPER_LAYER
+training_mode = constants.Training.LEARNING_RATE_TUNING # LEARNING_RATE_TUNING, NORMAL, K_FOLD, UPPER_LAYER
 
 cutout = False
-rgb_images = True # using gray scale images as input
+rgb_images = False # using gray scale images as input
 contrast_DA = False # data augmentation with contrast
 clinical_data = False
-use_layer = False
+use_layer = True # use the number of the layer as an input feature
 num_classes = 2
 
-use_pretrained_weights = True # if True, will load weights from path_to_weights if it exists
+use_pretrained_weights = False # if True, will load weights from path_to_weights if it exists
 weight_folder = "conv_00_3cls_slice_no_clin_no_layer_rgb_seq[t1c]_normal_DA_pretrain_rough_normal_run_2025_07_06_13_53_53/fold_0" + "/saved_weights.weights.h5"
 path_to_weights = constants.path_to_logs / weight_folder #constants.path_to_logs / "conv_00_2cls_slice_no_clin_no_layer_gray_seq[t1c]_normal_DA_pretrain_fine_normal_run_2025_04_24_13_22_27/fold_0/saved_weights.weights.h5"
 
