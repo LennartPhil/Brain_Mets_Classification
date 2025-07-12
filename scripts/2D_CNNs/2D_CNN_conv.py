0198b5b4a1b582dc6725139abe7adaa4a16b2088
Lennart Philipp <lennart-philipp@outlook.de>
2025-07-12 16:03:19 +0200

    sets training_mode to NORMAL

--- scripts/2D_CNNs/2D_CNN_conv.py
+++ scripts/2D_CNNs/2D_CNN_conv.py
@@ -25,7 +25,7 @@ print("tensorflow_setup successful")
 
 # --- Configuration ---
 dataset_type = constants.Dataset.NORMAL # PRETRAIN_ROUGH, PRETRAIN_FINE, NORMAL
-training_mode = constants.Training.LEARNING_RATE_TUNING # LEARNING_RATE_TUNING, NORMAL, K_FOLD, UPPER_LAYER
+training_mode = constants.Training.NORMAL # LEARNING_RATE_TUNING, NORMAL, K_FOLD, UPPER_LAYER
 
 cutout = False
 rgb_images = True # using gray scale images as input
