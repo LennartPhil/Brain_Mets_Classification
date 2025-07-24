a3c16232cc7d7998e33b24c03bc9f173f466d754
Lennart Philipp <lennart-philipp@outlook.de>
2025-07-24 15:33:15 +0200

    sets training_mode to K_FOLD, clinical_Data False, num_classes 2, selected_sequences t1c, learning_rate 0.003

--- scripts/2D_CNNs/2D_CNN_conv.py
+++ scripts/2D_CNNs/2D_CNN_conv.py
@@ -31,21 +31,21 @@ print(f"{len(gpus)} GPU(s) detected.")
 
 # --- Configuration ---
 dataset_type = constants.Dataset.NORMAL # PRETRAIN_ROUGH, PRETRAIN_FINE, NORMAL
-training_mode = constants.Training.NORMAL # LEARNING_RATE_TUNING, NORMAL, K_FOLD, UPPER_LAYER
+training_mode = constants.Training.K_FOLD # LEARNING_RATE_TUNING, NORMAL, K_FOLD, UPPER_LAYER
 
 cutout = False
 rgb_images = False # using gray scale images as input
 contrast_DA = False # data augmentation with contrast
-clinical_data = True
+clinical_data = False
 use_layer = False # use the number of the layer as an input feature
-num_classes = 5
+num_classes = 2
 
 use_pretrained_weights = False # if True, will load weights from path_to_weights if it exists
 weight_folder = "conv_00_3cls_slice_no_clin_no_layer_rgb_seq[t1c]_normal_DA_pretrain_rough_normal_run_2025_07_06_13_53_53/fold_0" + "/saved_weights.weights.h5"
 path_to_weights = constants.path_to_logs / weight_folder #constants.path_to_logs / "conv_00_2cls_slice_no_clin_no_layer_gray_seq[t1c]_normal_DA_pretrain_fine_normal_run_2025_04_24_13_22_27/fold_0/saved_weights.weights.h5"
 
 # --- Select Sequences ---
-selected_sequences = ["t1", "t1c", "t2", "flair", "mask"] #["t1", "t1c", "t2", "flair", "mask"]
+selected_sequences = ["t1c"] #["t1", "t1c", "t2", "flair", "mask"]
 
 if dataset_type == constants.Dataset.PRETRAIN_ROUGH:
     num_classes = 3
@@ -93,7 +93,7 @@ if training_mode == constants.Training.LEARNING_RATE_TUNING:
     training_epochs = constants.LEARNING_RATE_EPOCHS #400
 else:
     training_epochs = constants.MAX_TRAINING_EPOCHS #2500 #10
-learning_rate = 0.001
+learning_rate = 0.003
 
 # Regularization
 dropout_rate = 0.4
