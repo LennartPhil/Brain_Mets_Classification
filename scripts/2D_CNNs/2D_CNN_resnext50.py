cea406a374fe0ef62bc13d07ae60d2c5b27c1336
Lennart Philipp <lennart-philipp@outlook.de>
2025-10-09 12:25:03 +0200

    sets dataset_type to NORMAL, sets training_mode to LEARNING_RATE_TUNING, sets clinical_data to False, sets use_pretrained_weights to True, adjusts path_to_weights

--- scripts/2D_CNNs/2D_CNN_resnext50.py
+++ scripts/2D_CNNs/2D_CNN_resnext50.py
@@ -27,21 +27,20 @@ print(f"{len(gpus)} GPU(s) detected.")
 # print("tensorflow_setup successful")
 
 # --- Configuration ---
-dataset_type = constants.Dataset.PRETRAIN_FINE # PRETRAIN_ROUGH, PRETRAIN_FINE, NORMAL
-training_mode = constants.Training.NORMAL # LEARNING_RATE_TUNING, NORMAL, K_FOLD, UPPER_LAYER
+dataset_type = constants.Dataset.NORMAL # PRETRAIN_ROUGH, PRETRAIN_FINE, NORMAL
+training_mode = constants.Training.LEARNING_RATE_TUNING # LEARNING_RATE_TUNING, NORMAL, K_FOLD, UPPER_LAYER
 
 START_FOLD = 0
 
 cutout = False
 rgb_images = False # using gray scale images as input
 contrast_DA = False # data augmentation with contrast
-clinical_data = True
+clinical_data = False
 use_layer = False
 num_classes = 2
 
-use_pretrained_weights = False # if True, will load weights from path_to_weights if it exists
-weight_folder = "conv_00_3cls_slice_no_clin_no_layer_rgb_seq[t1c]_normal_DA_pretrain_rough_normal_run_2025_07_06_13_53_53/fold_0" + "/saved_weights.weights.h5"
-path_to_weights = constants.path_to_logs / weight_folder
+use_pretrained_weights = True # if True, will load weights from path_to_weights if it exists
+path_to_weights = "/home/lennart/work/weights/pretrain_fine/ResNeXt50/resnext50_00_2cls_slice_no_clin_no_layer_gray_seq[t1c]_normal_DA_pretrain_fine_normal_run_2025_10_03_21_17_54/fold_0" + "/saved_weights.weights.h5"
 
 # --- Select Sequences ---
 selected_sequences = ["t1c"] #["t1", "t1c", "t2", "flair", "mask"]
