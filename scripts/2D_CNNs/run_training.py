6fb10024586f4c083d11c44c139846a053537065
Lennart Philipp <lennart-philipp@outlook.de>
2025-12-29 23:28:49 +0100

    moves training order

--- scripts/2D_CNNs/run_training.py
+++ scripts/2D_CNNs/run_training.py
@@ -33,9 +33,10 @@ SCRIPTS_DIR: Optional[Path] = None
 JOBS: List[str] = [
     # "2D_CNN_conv.py",
     # "2D_CNN_resnet34.py",
-    "2D_CNN_resnet152.py",
+    #,
     "2D_CNN_resnext50.py",
     "2D_CNN_resnext101.py",
+    "2D_CNN_resnet152.py"
     #"2D_CNN_transfer_bit.py",
     #"2D_CNN_transfer_efficientv2.py",
     #"2D_CNN_transfer_inceptionv3.py",
