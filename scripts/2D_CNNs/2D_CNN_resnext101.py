5db7dbeadf0971dcc8aacb0d52e5feeaefe54c2b
Lennart Philipp <lennart-philipp@outlook.de>
2026-01-05 09:30:57 +0100

    sets training_mode to K_FOLD and decreases dropout_rate to 0.4

--- scripts/2D_CNNs/2D_CNN_resnext101.py
+++ scripts/2D_CNNs/2D_CNN_resnext101.py
@@ -28,7 +28,7 @@ print(f"{len(gpus)} GPU(s) detected.")
 
 # --- Configuration ---
 dataset_type = constants.Dataset.NORMAL # PRETRAIN_ROUGH, PRETRAIN_FINE, NORMAL
-training_mode = constants.Training.NORMAL # LEARNING_RATE_TUNING, NORMAL, K_FOLD, UPPER_LAYER
+training_mode = constants.Training.K_FOLD # LEARNING_RATE_TUNING, NORMAL, K_FOLD, UPPER_LAYER
 
 START_FOLD = 0
 
@@ -95,7 +95,7 @@ else:
 learning_rate = 0.03
 
 # Regularization
-dropout_rate = 0.45 #constants.REGULAR_DROPOUT_RATE #0.4
+dropout_rate = 0.4 #constants.REGULAR_DROPOUT_RATE #0.4
 l2_regularization = constants.REGULAR_L2_REGULARIZATION
 
 codename = "resnext101_00"
